2c10845ed05ab19f3132ac3a206a44a99b629674
𝙍𝙤𝙣𝙯𝙯𝘿𝙚𝙫 - 2026 <lebarl313@gmail.com>
2026-09-03 13:29:48 +0700

    Add basic user authentication in main.cpp
    Implement a simple login system with username and password checks.

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    //tipe datanya
+    string n,pw;
+    cout<< "masukkan nama anda ";
+    cin>> n;
+    cout<< "masukkan pw anda ";
+    cin>> pw;
+    
+    if (n=="ronzz" && pw=="ronzz123") {
+        cout<< "selamat datang bos ";
+        cout<< n;
+    }
+  else if (n=="rafi" && pw=="rafitzy7") {
+      cout<< "halo owner ";
+      cout<< n;
+  }
+   else {
+    cout<< "lu siapa mpruy";
+   }
+    return 0;
+}
